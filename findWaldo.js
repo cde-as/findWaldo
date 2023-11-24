0f8466c194275a9af8c4db50a215aaec738b81c3
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-23 20:58:15 -0800

    fixed the syntax errors

--- findWaldo.js
+++ findWaldo.js
@@ -3,14 +3,14 @@ const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
     let name = names[i];
     if (name === "Waldo") {
-      found();   // execute callback
-      return i;
+      //console.log(i);
+      found(i);   // this passes the index of Waldo to the func actionWhenFound
     }
   }
 };
 
-const actionWhenFound = function() {
-  console.log("Found him at index " + findWaldo(i));
+const actionWhenFound = function(index) { // Initializinf the index parameter is important so that we can return the value of found(i) into this scope
+  console.log("Found him at index " + index);
 };
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
